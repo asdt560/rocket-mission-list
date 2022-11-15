0e9645aac20714f7be449050081c639bfb6ad3ae
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-15 18:42:31 -0500

    Add myProfile/cancel reservation rocket feature

--- src/components/MyRockets.js
+++ src/components/MyRockets.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { ExternalLink } from 'react-external-link';
 
+import { changeRocketStatus } from '../redux/rockets/rocketsSlice';
 import './MyRockets.css';
 
 const MyRockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer.rockets);
   const activeRockets = rockets.filter((rocket) => rocket.reserved === true);
+
+  const dispatch = useDispatch();
+
   const reservedRockets = activeRockets.map((rocket) => (
     <li key={rocket.id} className="reservedRockets-item">
       <p>{rocket.name}</p>
       <ExternalLink href={rocket.wikipedia}>
         <span>Read more</span>
       </ExternalLink>
+      <button
+        type="button"
+        style={{ height: 30 }}
+        onClick={() => { dispatch(changeRocketStatus(rocket.id)); }}
+      >
+        Cancel Reservation
+      </button>
     </li>
   ));
 
