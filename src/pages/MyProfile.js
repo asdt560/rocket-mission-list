c333f533e3a26cebbf3ced93689bc3832b91766d
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-11-15 13:54:59 -0300

    Display joined missions on MyProfile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>
-    My Profile goes here!
-  </div>
-);
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missionsreducer.missions);
+  const activemissions = missions.filter((mission) => mission.joined === true);
+  return (
+    <div>
+      <div>
+        <h2>My Missions</h2>
+        {activemissions.map((mission) => (
+          <div key={mission.mission_id}>
+            {mission.mission_name}
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>My Rockets</h2>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
