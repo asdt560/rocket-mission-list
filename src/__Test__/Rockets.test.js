663757e424be538daef60aba0ffc41b2359c4b1d
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-11-17 12:15:02 -0300

    Fix test issues

--- src/__Test__/Rockets.test.js
+++ src/__Test__/Rockets.test.js
@@ -3,7 +3,7 @@ import { screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import App from '../App';
-import { renderWithProviders } from '../utils/utils-for-tests';
+import renderWithProviders from '../utils/utils-for-tests';
 
 describe('Rockets page ', () => {
   test('renders rockets', async () => {
